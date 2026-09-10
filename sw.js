9433e911a8c6e1183e7a425ab27a7ea2676ac938
Leon505 <leonpastora@gmail.com>
2026-09-10 09:27:56 -0600

    Update CACHE_NAME to force app update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ¡REGLA DE ORO!: Cada vez que modifiques tu app en GitHub, 
 // debes cambiar este número (v2, v3, v4...) para forzar la actualización.
-const CACHE_NAME = 'comasa-madurez-v3'; 
+const CACHE_NAME = 'comasa-madurez-v4'; 
 
 const urlsToCache = [
   './',
